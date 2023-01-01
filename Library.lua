5d314d95a926f1c89950ff674bf3f459260e045b
Kiwiwyi <121684294+Kiwiwyi@users.noreply.github.com>
2023-01-02 00:41:18 +0900

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -10,7 +10,7 @@ iRay  | Programming
 
 
 
-local Release = "Beta 7R"
+local Release = "Kiwi's GUI"
 local NotificationDuration = 6.5
 local RayfieldFolder = "Rayfield"
 local ConfigurationFolder = RayfieldFolder.."/Configurations"
